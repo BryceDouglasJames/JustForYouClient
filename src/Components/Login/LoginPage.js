a0c75eb324f965a2b2f7512b321d9d30bfeb9d98
Ziad Zakaria <zakariaz1@newpaltz.edu>
2021-02-20 12:59:39 -0500

    commit1

--- src/Components/Login/LoginPage.js
+++ src/Components/Login/LoginPage.js
@@ -4,38 +4,41 @@ import axios from 'axios'
 class LoginPage extends Component{
 
     state = {
-        name:'',
-        pass:''
+        username:'',
+        password:'',
     }
-
 
     handleFormSubmit( event ) {
         event.preventDefault();
 
         let logindata = new FormData();
-        logindata.append('name', this.state.name);
-        logindata.append('pass', this.state.pass);
+        logindata.append('username', this.state.username);
+        logindata.append('password', this.state.password);
 
-        //change this ip address
+        //change this ip address to Xampp's
         axios.post("http://192.168.64.2/JustForYouAPI/public/login", logindata)
         .then(resp=> console.log(resp))
         .catch(err=>console.log(err));
 
     }
 
+    // renders a username and password login form
     render(){
         return(
             <>
-            <form>
-                <label for="name">Name</label><br></br>
-                <input type="text" id="user" name="name" 
+            <form align="center">
+                <label for="username">Username or Email</label><br></br>
+                <input type="Username" placeholder="Enter your e-mail address" 
+                
                     onChange={e => this.setState({ name: e.target.value })}/>
                 <br></br><br></br>
-                <label for ="pass">Password</label><br></br>
-                <input type="password" name = "pass"
-                    onChange={e => this.setState({ pass: e.target.value })}/>
+                <label id ="password">Password</label><br></br>
+                <input type="password" name = "password" placeholder="Enter password"
+                    onChange={e => this.setState({ password: e.target.value })}/>
                 <br></br><br></br>
-                <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Add user" />
+                <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Sign In"/>
+                &nbsp; &nbsp;
+                <input type="submit" onClick={e => this.handleFormSubmit(e)} value="Sign Up"/>
             </form>
             </>
         )
